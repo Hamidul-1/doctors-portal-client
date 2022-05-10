6e15e231bbc292d2433040299d19a185555f97b5
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-10 22:23:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="about" element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
