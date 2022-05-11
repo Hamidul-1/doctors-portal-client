6ff6121a1cbf9f19bacca46a14a3da98ad56e16d
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-11 14:33:51 +0600

    Footer added

--- src/Pages/Home/Exceptional.js
+++ src/Pages/Home/Exceptional.js
@@ -4,12 +4,12 @@ import PrimaryButton from '../Shared/PrimaryButton';
 
 const Exceptional = () => {
     return (
-        <div class="hero min-h-screen px-24 grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
-            <div class="hero-content flex-col lg:flex-row">
-                <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+        <div className="hero min-h-screen px-24 grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={treatment} className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <PrimaryButton>Get Started</PrimaryButton>
                 </div>
             </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import Exceptional from './Exceptional';
+import Footer from './Footer';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
@@ -16,6 +17,7 @@ const Home = () => {
             <Exceptional></Exceptional>
             <MakeAppointment></MakeAppointment>
             <Testimonials></Testimonials>
+            <Footer></Footer>
         </div>
     );
 };
