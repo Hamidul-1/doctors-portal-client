2e6773f1a0df9518bbefb9605711284a69d3abc9
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-11 12:13:00 +0600

    servic added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Banner from './Banner';
+import Exceptional from './Exceptional';
 import Info from './Info';
+import Services from './Services';
 
 
 const Home = () => {
     return (
-        <div>
+        <div className='px-12'>
             <Banner></Banner>
             <Info></Info>
+            <Services></Services>
+            <Exceptional></Exceptional>
         </div>
     );
 };
