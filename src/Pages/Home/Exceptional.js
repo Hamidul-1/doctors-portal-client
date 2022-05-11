c306f79e931e66ffb177b03b78a86b3d53979273
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-11 12:22:38 +0600

    exceptional added

--- src/Pages/Home/Exceptional.js
+++ src/Pages/Home/Exceptional.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import treatment from '../../assets/images/treatment.png'
 
 const Exceptional = () => {
     return (
-        <div>
-            <h2>Exceptional</h2>
+        <div class="hero min-h-screen px-24">
+            <div class="hero-content flex-col lg:flex-row">
+                <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                </div>
+            </div>
         </div>
     );
 };
