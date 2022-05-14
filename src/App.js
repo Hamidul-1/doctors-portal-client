b38c711b887ed1e22aa3000f559af231990d7345
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-14 13:14:10 +0600

    Created BookingModule

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        {/* <Route path="home" element={<Home></Home>}></Route> */}
+        <Route path="home" element={<Home></Home>}></Route>
         <Route path="about" element={<About></About>}></Route>
         <Route path='appointment' element={<Appointment></Appointment>}></Route>
         <Route path='login' element={<Login></Login>}></Route>
