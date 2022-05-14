978500c2223e60f78eaa9d5c8544d4a8cbd46d77
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-14 21:19:02 +0600

    added firebase.init.js

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -8,14 +8,14 @@ const AvailableAppointments = ({ date }) => {
     const [treatment, setTreatment] = useState(null);
 
     useEffect(() => {
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
 
     return (
-        <div>
-            <h4 className='text-xl text-primary text-center'>Available Appointments on {format(date, 'PP')}</h4>
+        <div className='my-10'>
+            <h4 className='text-xl text-primary text-center my-10'>Available Appointments on {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
                     services.map(service => <Service
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -9,6 +9,8 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
         event.preventDefault();
         const slot = event.target.slot.value;
         console.log(_id, name, slot);
+
+        // to close the modal
         setTreatment(null);
     }
     return (
