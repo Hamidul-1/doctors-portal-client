e158d09f657791b064ffcbc9f7791a610f7a97d2
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-19 20:48:23 +0600

    Make user an admin

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -15,7 +15,7 @@ const MyAppointments = () => {
             fetch(`http://localhost:5000/booking?patient=${user.email}`, {
                 method: 'GET',
                 headers: {
-                    'authorization': `Bearer ${localStorage.getItem('access Token')}`
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
                 }
             })
                 .then(res => {
--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -5,7 +5,7 @@ import UserRow from './UserRow';
 
 const Users = () => {
     // react query
-    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/user',{
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user',{
         method: 'GET',
         headers:{
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
@@ -33,6 +33,7 @@ const Users = () => {
                             users.map(user => <UserRow
                                 key={user._id}
                                 user={user}
+                                refetch={refetch}
                             ></UserRow>)
                         }
                     </tbody>
